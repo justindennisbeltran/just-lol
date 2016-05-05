f20cf0ad9710986357e40f49820ac4b26649d707
Justin Beltran <justindennisbeltran@justin-mbp.local>
2016-05-06 00:43:03 +1000

    feat(select/region-dropdown): Create

---
+++ app/components/select/region-dropdown.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'select',
+
+  optionLabelPath: 'name',
+  optionValuePath: 'code',
+
+  regions: [
+    { name: 'Brazil', code: 'BR', platformId: 'BR1', host: 'br.api.pvp.net' },
+    { name: 'EU Nordic & East', code: 'EUNE', platformId: 'EUN1', host: 'eune.api.pvp.net' },
+    { name: 'EU West', code: 'EUW', platformId: 'EUW1', host: 'euw.api.pvp.net' },
+    { name: 'Japan', code: 'JP', platformId: 'JP1', host: 'jp.api.pvp.net' },
+    { name: 'Korea', code: 'KR', platformId: 'KR', host: 'kr.api.pvp.net' },
+    { name: 'Latin America North', code: 'LAN', platformId: 'LA1', host: 'lan.api.pvp.net' },
+    { name: 'Latin America South', code: 'LAS', platformId: 'LA2', host: 'las.api.pvp.net' },
+    { name: 'North America', code: 'NA', platformId: 'NA1', host: 'na.api.pvp.net' },
+    { name: 'Oceania', code: 'OCE', platformId: 'OC1', host: 'oce.api.pvp.net' },
+    { name: 'Turkey', code: 'TR', platformId: 'TR1', host: 'tr.api.pvp.net' },
+    { name: 'Russia', code: 'RU', platformId: 'RU', host: 'ru.api.pvp.net' },
+    { name: 'Public Beta Environment', code: 'PBE', platformId: 'PBE1', host: 'pbe.api.pvp.net' }
+  ],
+
+  change(event) {
+    let name = event.target.selectedOptions[0].text;
+    this.attrs.changeAction(name);
+  }
+});
