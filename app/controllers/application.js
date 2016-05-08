6d05bce7465f7e821eef4634c8abb82a95d787ae
Justin Beltran <justindennisbeltran@justin-mbp.local>
2016-05-08 22:49:54 +1000

    feat(application): Fetch summoner data.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,8 @@ import config from '../config/environment';
 export default Ember.Controller.extend({
   apiKey: Ember.computed(() => config.apiKey),
 
+  summonerName: 'Just',
+
   regions: [
     { name: 'Brazil', code: 'BR', platformId: 'BR1', host: 'br.api.pvp.net' },
     { name: 'EU Nordic & East', code: 'EUNE', platformId: 'EUN1', host: 'eune.api.pvp.net' },
@@ -29,5 +31,24 @@ export default Ember.Controller.extend({
       this.set('selectedRegionCode', Ember.get(value, 'code'));
       return value;
     }
-  })
+  }),
+
+  actions: {
+    getSummoner() {
+      const host = this.get('selectedRegion.host');
+      const code = this.get('selectedRegionCode').toLowerCase();
+      const summonerName = this.get('summonerName');
+
+      let requestUrl = `https://${host}/api/lol/${code}/v1.4/summoner/by-name/${summonerName}?api_key=${config.apiKey}`;
+
+      let promise = new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.$.getJSON(requestUrl, (response) => {
+          let summonerData = response[summonerName.toLowerCase()];
+          Ember.Logger.debug(summonerData);
+
+          this.set('summoner', Ember.Object.create(summonerData));
+        });
+      });
+    }
+  }
 });
